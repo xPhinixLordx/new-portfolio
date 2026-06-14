105bde3c00f532401f361339744ce4022a17172e
xPhinixLordx <cainantracy@gmail.com>
2026-06-13 22:46:58 -0700

    js: finished js

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-// 
-// 
-// 
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+    document.body.classList += " dark-theme";
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -23,4 +45,13 @@ function contact(event) {
                 "The email service is temporarily unavailable. Please contact me directly on cainantracy@gmail.com"
             )
         })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
